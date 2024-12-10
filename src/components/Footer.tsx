3cd0968aaab96cdb33e666310712c948c34e82eb
devskale <dev@skale.io>
2024-12-10 14:03:51 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ export function Footer() {
       <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8 mt-4">
         <div className="flex justify-between items-center">
           <p className="text-gray-600 text-sm">
-            Projektpartner: <span className="font-semibold">
+            Partner: <span className="font-semibold">
               <img
                 src="/src/assets/images/sharkbau-end-plain-svg.png"
                 alt="sharkbau Logo"
-                className="h-6 w-auto inline"
+                className="h-6 w-auto inline filter grayscale contrast-[1]"
               />
             </span>
           </p>
